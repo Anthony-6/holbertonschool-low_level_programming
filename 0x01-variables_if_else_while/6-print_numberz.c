7326634b1f2c8085e63af9bf70150c55464eaf05
Anthony-6 <3588@holbertonschool.com>
2021-10-02 15:51:22 +0200

    added C file that print digit number from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print number from 0 to 9 on base 10
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+    putchar(n);
+}
+putchar('\n');
+return (0);
+}
