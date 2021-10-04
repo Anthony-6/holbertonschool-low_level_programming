6c87e930fe79f58fcdb8eb5028b8e49beedf2ce5
Anthony-6 <3588@holbertonschool.com>
2021-10-04 15:54:43 +0200

    please work

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,20 @@
 * Returne - always sucess (0)
 */
 int main()
+
 {
 int n1;
 int n2;
 
-for(n1 = '0'; n1 <= '9'; n1++)
+for (n1 = '0'; n1 <= '9'; n1++)
 {
-for(n2 = '0'; n2 <= '9'; n2++)
-{  
-if(n1<n2)
+for (n2 = '0'; n2 <= '9'; n2++)
+{
+if (n1<n2)
 {
 putchar(n1);
 putchar(n2);
-if(n1 != '8')
+if (n1 != '8')
 {
 putchar (',');
 putchar (' ');
