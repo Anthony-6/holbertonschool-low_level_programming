297893bb79e37f843182083a3b07c7eafcb37d56
Anthony-6 <3588@holbertonschool.com>
2021-10-04 15:51:38 +0200

    finnaly

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* print number from 0 to 99 but without repetition
+* Returne - always sucess (0)
+*/
+int main()
+{
+char n1;
+char n2;
+
+for(n1 = '0'; n1 <= '9'; n1++)
+{
+for(n2 = '0'; n2 <= '9'; n2++)
+{  
+if(n1<n2)
+{
+putchar(n1);
+putchar(n2);
+if(n1 != '8')
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+putchar ('\n');
+return (0);
+}
