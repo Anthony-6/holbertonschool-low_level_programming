6588a2c600fc84762b68c61c1d2b45fec5dec4e0
Anthony-6 <3588@holbertonschool.com>
2021-10-05 14:06:45 +0200

    please work

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,16 +2,19 @@
 
 /**
  * main - entry point
- * return - always sucess (0)
+ * Return: always sucess (0)
  * this program print "_putchar"
- *
  *
  */
 int main(void)
 {
 int c;
-char str [8] = "_putchar";
-for (c = 0 ; c <= 9; c++);
-_putchar(str[c]);
-return(0);
+char str[8] = "_putchar";
+
+for (c = 0; c <= 9; c++)
+{
+_putchar (str[c]);
+}
+_putchar ('\n');
+return (0);
 }
