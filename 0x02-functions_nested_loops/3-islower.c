34d21c152421a8a6a0e5cc183ddeaf702ded0ff2
Anthony-6 <3588@holbertonschool.com>
2021-10-06 12:56:59 +0200

    corrected this cool program

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include <ctype.h>
 
 /**
- * islower - check if a character is lower than an other one
+ * _islower - check if a character is lower than an other one
  *
- * Return - 1 if it's lower, 0 if not
+ * Return - _islower always sucess (0)
+ * c is the 
  */
 int _islower(int (c))
 
 {
-	if (islower (c))
+	if(islower (c))
 	return (1);
 	else
 		return (0);
