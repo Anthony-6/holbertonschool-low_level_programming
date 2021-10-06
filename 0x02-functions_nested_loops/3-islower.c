8660b947933ef9316747e090a6af7fa53892cae5
Anthony-6 <3588@holbertonschool.com>
2021-10-06 12:44:12 +0200

    added new cool program

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+
+/**
+ * islower - check if a character is lower than an other one
+ *
+ * Return - 1 if it's lower, 0 if not
+ */
+int _islower(int (c))
+
+{
+	if (islower (c))
+	return (1);
+	else
+		return (0);
+}
