466888adfd784395e0ec534579ec0ddc6ae023d0
Anthony-6 <3588@holbertonschool.com>
2021-10-07 13:51:05 +0200

    added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,17 @@
 #include "main.h"
+
+/**
+ * print_numbers - print number from 0 to 9
+ *
+ * Return: no
+ */
+void print_numbers(void)
+{
+	int l;
+
+	for (l = 0; l <= 9; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+}
