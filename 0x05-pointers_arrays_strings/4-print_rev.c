5d354d34d5d5b2fd0afe41e4bcc795c5f3e35275
Anthony-6 <3588@holbertonschool.com>
2021-10-12 11:16:18 +0200

    added new functions

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - print a chain caracter but reversed
+ * @s: caracter chain
+ * Return: no
+ */
+void print_rev(char *s)
+{
+	int j;
+
+	j = strlen(s) - 1;
+
+		while (s[j] >= 0)
+		{
+			_putchar (s[j]);
+			j--;
+		}
+	_putchar ('\n');
+}
