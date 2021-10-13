8950343e83770dc0aa677bf7ff76bb34e1bf594f
Anthony-6 <3588@holbertonschool.com>
2021-10-13 13:29:19 +0200

    created new dir, created new readme for this directory and created a functions that print hello world

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - pointer for the string to print
+ * @dest:  destination pointer
+ * @src: pointer
+ * Return: strcat
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
