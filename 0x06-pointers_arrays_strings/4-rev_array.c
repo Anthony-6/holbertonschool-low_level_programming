e3c0647d7010156f05de5b8a5437400d0eb28045
Anthony-6 <3588@holbertonschool.com>
2021-10-14 12:37:49 +0200

    corrected description for file nb4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,15 +4,16 @@
 /**
  * reverse_array - reverse array
  * @n: integer value
- * @i: integer value
+ * i: integer value
  * @a: integer value
+ * h: int value
  * Return: no
  */
 void reverse_array(int *a, int n)
 {
 	int h;
 	int i;
-	
+
 	i = 0;
 	while (i < n / 2)
 	{
