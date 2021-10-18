987da26db07478062451b78ccc2216928be1ac21
Anthony-6 <3588@holbertonschool.com>
2021-10-18 11:31:38 +0200

    added function four

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - compare two string
+ *
+ * @s: string
+ * @accept: compare I think
+ * Return: strspn
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
