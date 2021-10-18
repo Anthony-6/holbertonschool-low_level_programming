610b17b3b8e29b4f990824d2575545c61741f0f8
Anthony-6 <3588@holbertonschool.com>
2021-10-18 10:48:50 +0200

    add function n°2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - start at the first occurence of the char c
+ *
+ * @s: start string
+ * @c: character selected for to serve of first occurence
+ * Return: strchr
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
