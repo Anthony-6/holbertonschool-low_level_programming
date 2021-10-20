d77098815f9df58b3828f87f6b2b510b12cd0259
Anthony-6 <3588@holbertonschool.com>
2021-10-20 14:06:57 +0200

    added functino

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raised the power of an int
+ * @x:base
+ * @y: power
+ * Return: power
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y % 2 == 0)
+	{
+		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
+	}
+}
