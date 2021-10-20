52c8e1c325a0f12eaae352afd7d5bf91ceb0a63a
Anthony-6 <3588@holbertonschool.com>
2021-10-20 10:29:45 +0200

    corrected betty coding style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,15 @@
  * _puts_recursion - recursion
  * @s: pointer
  * Return: permit to stop the recursive
- * 
+ *
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar ('\n');
-    return;
-    }
-_putchar (*s);
-_puts_recursion (s+1);
+	if (*s == '\0')
+	{
+		_putchar ('\n');
+		return;
+	}
+	_putchar (*s);
+	_puts_recursion (s+1);
 }
