ceb7501e07fc0d6b155281f4e04e601c3c4a2c68
Anthony-6 <3588@holbertonschool.com>
2021-10-22 12:03:53 +0200

    corrected betty

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,16 +4,16 @@
  * main - calcul the number of string
  * @argc: string
  * @argv: string size
- * print - increment 
+ * print - increment
  * Return: 0
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-    int print;
+	int print;
 
-    for (print = 0; print < argc; print++)
-    {
-	printf("%s\n", argv[print]);
-    }
-    return (0);
+	for (print = 0; print < argc; print++)
+	{
+		printf("%s\n", argv[print]);
+	}
+	return (0);
 }
