79975aa3d11dd8f15cb72d7ac93a6328058ef15b
Anthony-6 <3588@holbertonschool.com>
2021-10-25 10:08:13 +0200

    add new function

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+ /**
+  * 
+  * 
+  * 
+  * 
+  */
+ int main(int argc, char *argv[])
+ {
+    if (argc == 3)
+    {
+    int a = atoi(argv[1]), b = atoi(argv[2]);
+    
+    printf("%d\n", a * b);
+    return (0);
+    }
+    else
+{
+        printf("Error\n");
+        return (1);
+}
+}
