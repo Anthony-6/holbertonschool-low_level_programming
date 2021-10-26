5d78b873a20526ba3aeec19a94d9d207a1ad2639
Anthony-6 <3588@holbertonschool.com>
2021-10-26 17:32:38 +0200

    corrected the entire file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * str_concat - concate two string
@@ -9,43 +10,39 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int i = 0, j = 0, k = 0, l = 0;
-char *s3 = 0;
+	int i = 0, j = 0, k = 0, l = 0;
+	char *s3;
 
-if (s1 == NULL)
-	i = 0;
-else
-{
-	while (s1[i])
-		i++;
-}
-if (s2 == NULL)
-	k = 0;
-else
+	if (s1 != NULL)
+	{
+		while (s1[i])
+			i++;
+	}
+
+	if (s2 != NULL)
 {
 	while (s2[k])
 		k++;
 }
-l = k + i + 1;
-s3 = (char *)malloc(sizeof(char) * l);
-if (s3 == NULL)
-{
-	return (NULL);
-}
-i = 0;
-while (s1[i] != '\0')
-{
-	s3[j] = s1[i];
-	i++;
-	j++;
-}
-k = 0;
-while (s2[k] != '\0')
-{
-	s3[j] = s2[k];
-	k++;
-	j++;
-}
-s3[j] = '\0';
-return (s3);
+
+	l = k + i + 1;
+	s3 = (char *)malloc(sizeof(char) * l);
+	if (s3 == NULL)
+		return (NULL);
+	i = 0;
+	while (s1 && s1[i] != '\0')
+	{
+		s3[j] = s1[i];
+		i++;
+		j++;
+	}
+	k = 0;
+	while (s2 && s2[k] != '\0')
+	{
+		s3[j] = s2[k];
+		k++;
+		j++;
+	}
+	s3[j] = '\0';
+	return (s3);
 }
