d7a19c62db56080d8bfe204de10a35ebd367dd90
Anthony-6 <3588@holbertonschool.com>
2021-11-02 10:06:41 +0100

    add new directory, new file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+i need to fill this one too
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DEF_DOG
+#define DEF_DOG
+/**
+ * struct dog - define structure for dog.h
+ * @name: pointer of char type
+ * @age: float
+ * @owner: pointer of char type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
