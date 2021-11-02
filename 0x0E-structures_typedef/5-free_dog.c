699124ca4a367f0fe4d59de417ff6701feed07b2
Anthony-6 <3588@holbertonschool.com>
2021-11-02 15:01:28 +0100

    fixed betty

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- * 
- * 
- * 
+ * free_dog - clear memory of name, owner and age
+ * @d: pointer
  */
 void free_dog(dog_t *d)
 {
-    if (d != 0)
-    free (d->name);
-    free (d);
-    free (d->owner);
+if (d != 0)
+{
+	free(d->name);
+	free(d);
+	free(d->owner);
+}
 }
