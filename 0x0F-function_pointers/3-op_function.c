b193feca2b68cccc728c45362986f8967980f8e3
Anthony-6 <3588@holbertonschool.com>
2021-11-04 10:35:46 +0100

    added new dir and new file

---
+++ 0x0F-function_pointers/3-op_function.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - additionnate two numer
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract two numer
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplicate two numer
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide two numer
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulate two numer
+ * @a: first number
+ * @b: second number
+ * Return: sum
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
