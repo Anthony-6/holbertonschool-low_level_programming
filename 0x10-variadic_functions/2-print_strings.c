e011a419697d9165028656814b80de43351857df
Anthony-6 <3588@holbertonschool.com>
2021-11-04 16:25:39 +0100

    added file nb2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,33 +1,28 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
- * print_strings - print string with commas betwin
+ * print_string - print string with commas betwin
  * @separator: point to defined commas
  * @n: number of string
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	char *str;
 	unsigned int i;
 	va_list pointer;
-
 	va_start(pointer, n);
 
 	for (i = 0; i < n; i++)
 	{
-		const char *str = va_arg(pointer, const char*);
-
-		printf("%s", str);
-		if (i != n - 1 && pointer != 0)
+		str = va_arg(pointer, char*);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (i != n - 1 && separator != NULL)
 		{
 			printf("%s", separator);
 		}
-
-	if (separator == NULL)
-		return;
-
-	if (n == 0)
-		printf("(nil)");
-
 		va_end(pointer);
 	}
 	printf("\n");
