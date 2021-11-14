db62d2a94a2cc8b0838e1a966309392e93a207c9
Anthony-6 <3588@holbertonschool.com>
2021-11-14 22:32:00 +0100

    added list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * list_lent - main function that return number of element of list_t list
+ * @h: list value
+ * count: incremante the loop
+ * Return: count value
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	for (count = 0; h != NULL; count++)
+		h = h->next;
+	return (count);
+}
