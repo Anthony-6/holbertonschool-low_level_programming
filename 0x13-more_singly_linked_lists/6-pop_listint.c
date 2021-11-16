a12d6edb6821f2af7ceaf3136d67d2af32c18d87
Anthony-6 <3588@holbertonschool.com>
2021-11-16 13:44:25 +0100

    add function -6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - main function
+ * @head: point to the list
+ * @delete: point to head
+ * res: result of delete
+ * Return: result or NULL if it fail
+ */
+int pop_listint(listint_t **head)
+{
+        listint_t *delete = *head;
+	int res = delete->n;
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	(*head) = (*head)->next;
+	free (delete);
+	return (res);
+}
