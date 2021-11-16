7b84926576575cee07b9448a84642e0b3293b8dd
Anthony-6 <3588@holbertonschool.com>
2021-11-16 13:13:59 +0100

    add function number 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * free_listint - free the list
+ * @head: pointer to the list
+ * memfree: point the the list then free it
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *memfree;
+
+    if (head == NULL)
+    return;
+
+	while (*head != NULL)
+	{
+		memfree = *head;
+		*head = (*head)->next;
+		free(memfree);
+	}
+    head = NULL;
+}
