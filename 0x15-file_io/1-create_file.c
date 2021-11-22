a482aa7ed191b1a97b377f51bc1ed74cf8ffa82e
Anthony-6 <3588@holbertonschool.com>
2021-11-22 15:14:30 +0100

    add function 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - create a file
+ * @filename: the name of the file
+ * @text_content: the content of the file
+ * Return: -1/0 if fails or empty, and 1 if it's work
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_RDONLY | O_WRONLY | O_WRONLY, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
+	close(fd);
+	return (1);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to int b
+ * Return: The length of the string c
+ */
+
+int _strlen(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+		;
+	return (c);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,5 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+int create_file(const char *filename, char *text_content);
+int _strlen(const char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
