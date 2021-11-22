990b77b64f512a3765ea445d46d34dbda5fe5369
Anthony-6 <3588@holbertonschool.com>
2021-11-22 15:29:51 +0100

    add unction 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * append_text_to_file - add text a the end of a file
+ * @filename: the name of the file
+ * @text_content: the content of the file
+ * Return: -1/0 if fails or empty, and 1 if it's work
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+		write(fd, text_content, _strlen(text_content));
+
+	close(fd);
+	return (1);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to int b
+ * Return: The length of the string c
+ */
+
+int _strlen(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+		;
+	return (c);
+}
