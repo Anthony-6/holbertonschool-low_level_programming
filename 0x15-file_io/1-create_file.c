2cec43d35a26d35cc46ae950a752c692cec70227
Anthony-6 <3588@holbertonschool.com>
2021-11-22 15:20:53 +0100

    fixed the error, it was from the open line

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,14 +12,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDONLY | O_WRONLY | O_WRONLY, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
 	if (text_content)
 		write(fd, text_content, _strlen(text_content));
-        
+
 	close(fd);
 	return (1);
 }
