57239487f606ea9fe07818b88f219cc33308f2d3
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-10 13:33:10 +0100

    corrected file

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <inttypes.h>
+#include <string.h>
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
