e6b9e2089e5b19891f3abf2be2534b359b2d6a7d
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-28 23:59:09 +0100

    hotfix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,9 +6,8 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *ht =  malloc(sizeof(hash_table_t));
 
-	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 	ht->size = size;
