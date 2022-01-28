1b66eb0168b94c60389c5b73c8933fbf53fffea6
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-28 23:52:38 +0100

    add file

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table
+ * @size: size of the table
+ * Return: null if an error occured or the new hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+	ht->size = size;
+	ht->array = malloc(size * sizeof(hash_table_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+	return (ht);
+}
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - get the index of a given key
+ * Return: the index of the table
+ * @key: key needed to the get the index
+ * @size: size of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+	return (index);
+}
