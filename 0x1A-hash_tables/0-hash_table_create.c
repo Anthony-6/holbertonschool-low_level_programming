1c608597e62b29a0c6de0aa1ece11cec40301dec
Anthony-6 <anthony.lacoste5@gmail.com>
2022-01-29 00:05:09 +0100

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (ht == NULL)
 		return (NULL);
+
 	ht->size = size;
+
 	ht->array = malloc(size * sizeof(hash_table_t *));
 	if (ht->array == NULL)
 	{
